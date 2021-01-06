3aaa9b29831f84aa4234f9582ed36ddc71116e8d
jarbasal <jarbasai@mailfence.com>
2021-01-06 18:58:05 +0000

    is gui connected

--- ovos_utils/gui.py
+++ ovos_utils/gui.py
@@ -1,4 +1,5 @@
 from ovos_utils.system import is_installed, has_screen
+from ovos_utils.messagebus import wait_for_reply
 
 
 def can_display():
@@ -9,6 +10,12 @@ def is_gui_installed():
     return is_installed("mycroft-gui-app")
 
 
-def is_gui_connected():
-    # TODO bus api for https://github.com/MycroftAI/mycroft-core/pull/2682
+def is_gui_connected(bus=None):
+    # bus api for https://github.com/MycroftAI/mycroft-core/pull/2682
+    # send "gui.status.request"
+    # receive "gui.status.request.response"
+    response = wait_for_reply("gui.status.request",
+                              "gui.status.request.response", bus=bus)
+    if response:
+        return response.data["connected"]
     return False
