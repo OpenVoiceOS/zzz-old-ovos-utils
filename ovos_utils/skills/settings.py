26a109cdc1fb6274a7802c936bdb0cf670d3fa4e
jarbasal <jarbasai@mailfence.com>
2021-03-06 14:52:37 +0000

    better label

--- ovos_utils/skills/settings.py
+++ ovos_utils/skills/settings.py
@@ -5,25 +5,26 @@ def settings2meta(settings, section_name="Skill Settings"):
     for k, v in settings.items():
         if k.startswith("_"):
             continue
+        label = k.replace("-", " ").replace("_", " ").title()
         if isinstance(v, bool):
             fields.append({
                 "name": k,
                 "type": "checkbox",
-                "label": k,
+                "label": label,
                 "value": str(v).lower()
             })
         if isinstance(v, str):
             fields.append({
                 "name": k,
                 "type": "text",
-                "label": k,
+                "label": label,
                 "value": v
             })
         if isinstance(v, int):
             fields.append({
                 "name": k,
                 "type": "number",
-                "label": k,
+                "label": label,
                 "value": str(v)
             })
     return {
