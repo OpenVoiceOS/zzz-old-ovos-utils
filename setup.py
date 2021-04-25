f9592c084be0648fed9adc62b2e0b916ff7ce896
jarbasal <jarbasai@mailfence.com>
2021-04-26 00:15:34 +0100

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='ovos_utils',
-    version='0.0.9a1',
+    version='0.0.9a2',
     packages=['ovos_utils',
               'ovos_utils.intents',
               'ovos_utils.sound',
